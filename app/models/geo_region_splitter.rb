b6944a89d6eec963a344941a6f4a78cb5be2a770
Matt Green <mattgreenrocks@gmail.com>
2014-09-25 17:01:41 -0400

    Flesh out AnalysisWorker; clean up GeoRegionSplitter

--- app/models/geo_region_splitter.rb
+++ app/models/geo_region_splitter.rb
@@ -1,16 +1,11 @@
 module GeoRegionSplitter
-  RESOLUTIONS = [0.06, 0.05, 0.04, 0.03, 0.02, 0.01]
-
   # Returns a list of 0-n evenly-sized square GeoRegions contained within
-  # the pairs of lat/longs.
-  #
-  # Resolution is presumed to be an integer value in the range of 0-5, where
-  # 5 specifies maximum resolution, and 0 specifies minimum resolution.
-  def self.split(ne_lat, ne_long, se_lat, se_long, resolution)
-    increment = RESOLUTIONS[resolution]
+  # the lat/lon pairs of the analysis.
+  def self.split(region, resolution_mi)
+    increment = 0.011363636 * resolution_mi
 
-    lats = Range.new(ne_lat, se_lat, true).step(increment).to_a
-    longs = Range.new(ne_long, se_long, true).step(increment).to_a
+    lats = Range.new(region.se_lat, region.nw_lat, true).step(increment).to_a
+    longs = Range.new(region.nw_lon, region.se_lon, true).step(increment).to_a
 
     lats.product(longs).map do |lat, long|
       GeoRegion.new(nw_lat: lat,
