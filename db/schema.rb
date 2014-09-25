2e17376f3ee488e9129a76731f84797643848caf
John Fawcett <john.fawcett@kyrus-tech.com>
2014-09-25 11:36:56 -0600

    LocatedFoodSource belongs to Analysis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925165020) do
+ActiveRecord::Schema.define(version: 20140925173551) do
 
   create_table "analyses", force: true do |t|
     t.string   "name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140925165020) do
     t.decimal  "lon"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "analysis_id"
   end
 
   create_table "users", force: true do |t|
