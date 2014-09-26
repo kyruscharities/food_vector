2c71c57463591f6b7b13ee666ca287b223e2a09a
John Fawcett <john.fawcett@kyrus-tech.com>
2014-09-25 18:11:30 -0600

    Split geo into 900 blocks

--- app/models/geo_region_splitter.rb
+++ app/models/geo_region_splitter.rb
@@ -2,16 +2,18 @@ module GeoRegionSplitter
   # Returns a list of 0-n evenly-sized square GeoRegions contained within
   # the lat/lon pairs of the analysis.
   def self.split(region, resolution_mi)
-    increment = 0.011363636 * resolution_mi
+    lat_increment = (region.se_lat - region.nw_lat).abs / 30
+    lon_increment = (region.se_lon - region.nw_lon).abs / 30
+    # increment = 0.011363636 * resolution_mi
 
-    lats = Range.new(region.se_lat, region.nw_lat, true).step(increment).to_a
-    longs = Range.new(region.nw_lon, region.se_lon, true).step(increment).to_a
+    lats = Range.new(region.se_lat, region.nw_lat, true).step(lat_increment).to_a
+    longs = Range.new(region.nw_lon, region.se_lon, true).step(lon_increment).to_a
 
     lats.product(longs).map do |lat, long|
       GeoRegion.new(nw_lat: lat,
                     nw_lon: long,
-                    se_lat: lat + increment,
-                    se_lon: long + increment)
+                    se_lat: lat + lat_increment,
+                    se_lon: long + lon_increment)
     end
   end
 end
