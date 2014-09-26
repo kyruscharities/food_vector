c7dde82b7efcf66fc8be171b0471a070e26fa486
Chris Rothe <chris@redcanary.co>
2014-09-25 19:48:22 -0600

    allow rectangle drawing to set box for analysis

--- app/assets/javascripts/analyses.js
+++ app/assets/javascripts/analyses.js
@@ -39,6 +39,7 @@ $(document).ready(function () {
     $('body.analyses.new').ready(function () {
         console.log("loading new stuff");
         var handler, map;
+        var drawn_rectangle;
 
         var mapOptions = {
             zoom: 3,
@@ -48,5 +49,46 @@ $(document).ready(function () {
 
         map = new google.maps.Map(document.getElementById('map'),
             mapOptions);
+
+        var drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: google.maps.drawing.OverlayType.MARKER,
+            drawingControl: true,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: [
+                    google.maps.drawing.OverlayType.RECTANGLE
+                ]
+            },
+            markerOptions: {
+                icon: 'images/beachflag.png'
+            },
+            circleOptions: {
+                fillColor: '#ffff00',
+                fillOpacity: 1,
+                strokeWeight: 5,
+                clickable: false,
+                editable: true,
+                zIndex: 1
+            }
+        });
+        drawingManager.setMap(map);
+
+        google.maps.event.addListener(drawingManager, 'rectanglecomplete', function(rectangle) {
+            if(drawn_rectangle !== undefined) {
+                drawn_rectangle.setMap(null);
+            }
+            drawn_rectangle = rectangle;
+            ne = rectangle.getBounds().getNorthEast();
+            sw = rectangle.getBounds().getSouthWest();
+            nw_lat = ne.lat();
+            nw_lon = sw.lng();
+            se_lat = sw.lat();
+            se_lon = ne.lng();
+
+            $('#analysis_geo_region_attributes_nw_lat').val(nw_lat);
+            $('#analysis_geo_region_attributes_nw_lon').val(nw_lon);
+            $('#analysis_geo_region_attributes_se_lat').val(se_lat);
+            $('#analysis_geo_region_attributes_se_lon').val(se_lon);
+        });
     });
 })
