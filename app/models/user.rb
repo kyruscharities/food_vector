61a1d0a919be6f4c0e0085382e695d18de32d996
Chris Rothe <chris@redcanary.co>
2014-09-28 20:23:04 -0600

    add normal_user role by default when an account is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
   rolify
+
+  after_create :default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def default_role
+    add_role :normal_user
+  end
 end
