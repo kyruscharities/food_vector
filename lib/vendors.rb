86f9f9cb98d4f1546e538c5eb6f29eb363d7db49
Brian Beyer <brianebeyer@gmail.com>
2014-10-06 10:30:19 -0600

    find or create by for located food sources

--- lib/vendors.rb
+++ lib/vendors.rb
@@ -10,7 +10,7 @@ module Vendors
       Vendors.get_vendors(fs, analysis.geo_region)
     end
   end
-  
+
   def get_vendors(fs, geo, pagetok='')
     request_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" \
                   "key=#{ENV.fetch "GOOGLEMAPS_API_KEY"}" \
@@ -18,7 +18,7 @@ module Vendors
                   "&types=food" \
                   "&name=#{CGI.escape(fs.business_name)}" \
                   "&rankby=distance"
-    
+
     if pagetok != ""
       request_url = "#{request_url}&pagetoken=#{pagetok}"
     end
@@ -31,10 +31,10 @@ module Vendors
     rescue => e
       puts "ERROR: Unhandled Exception - #{e}"
     end
-    
+
     if response.code == 200
       data = JSON.parse(response.to_str)
-      
+
       if data['results'].length == 0
         puts "No results found for #{fs.business_name}. Breaking..."
         return
@@ -42,10 +42,10 @@ module Vendors
 
       for loc in data['results']
         puts "Located Food Source: #{loc}"
-        
+
         loc_lat = loc['geometry']['location']['lat']
         loc_lon = loc['geometry']['location']['lng']
-        
+
         if loc_lat > (geo.nw_lat + 0.1) or loc_lat < (geo.se_lat - 0.1)
           puts "breaking loop because too big or small of latitude"
           return
@@ -53,11 +53,11 @@ module Vendors
           puts "breaking loop because too big or small of long"
           return
         end
-        
-        LocatedFoodSource.create! lat: loc_lat,
-                                  lon: loc_lon,
-                                  price_rank: loc['price_level'],
-                                  food_source_id: fs.id
+
+        LocatedFoodSource.find_or_create_by! lat: loc_lat,
+                                             lon: loc_lon,
+                                             price_rank: loc['price_level'],
+                                             food_source_id: fs.id
       end
     else
       puts "ERROR: Request returned code - #{response.code}"
@@ -65,7 +65,7 @@ module Vendors
 
     Vendors.get_vendors(fs, geo, pagetok=data['next_page_token']) if data['next_page_token']
   end
-  
+
   module_function :get_food_sources_by_region
   module_function :get_vendors
 end
